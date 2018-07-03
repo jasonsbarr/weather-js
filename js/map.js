a57d180c1522352d59f121ec6296a3c641fd13b6
Jason Barr <jason@jasonsbarr.com>
2018-07-03 02:03:15 -0500

    Fetch coordinates info from MapQuest API based on user-provided city and state

---
+++ js/map.js
@@ -0,0 +1,32 @@
+class Map {
+    /**
+     * Initialize Map object to communicate with MQ API
+     */
+    constructor() {
+        // Set API key and get coordinates
+        fetch('env.json').then(response => response.json())
+        .then(env => this.apiKey = env.mqApiKey)
+        .catch(error => document.body.innerHTML = "App failed to initialize");
+    }
+
+    /**
+     * Fetch coordinates for city + state from MapQuest API
+
+     */
+    async fetchCoordinates() {
+        // Set URL to city + state
+        let url = `http://www.mapquestapi.com/geocoding/v1/address?key=${this.apiKey}&location=${this.city}%2C${this.state.replace(' ', '+')}`;
+
+        // Get response from MQ server
+        let coordsResponse = await fetch(url)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+        
+        this.coords = coordsResponse.results[0].locations[0].latLng;
+    }
+
+    setCityAndState(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
